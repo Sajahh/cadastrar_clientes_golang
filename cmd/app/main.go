f7320d2927ff95d9a7379b47a920fa42a2bb2b08
Sajahh <mariavaicomoutras@hotmail.com>
2024-03-06 10:08:28 -0300

    feat: finalizando testes e programa, cadastro retroativo de clientes completo

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -7,12 +7,11 @@ import (
 	"meu_projeto_omie_gennera/pkg/utils"
 	"os"
 	"strings"
-	"sync"
+	"time"
 
 	"github.com/joho/godotenv"
 )
 
-// REVIEW - Testar se ID esta sendo armazenado
 func main() {
 	err := godotenv.Load()
 	if err != nil {
@@ -36,45 +35,37 @@ func main() {
 
 	idPersonsStr := os.Getenv("ID_PERSON")
 	idPersons := strings.Split(idPersonsStr, ",")
-	var wg sync.WaitGroup
 
 	var idsComErro []string
-	var mutex sync.Mutex
 
 	for _, idStr := range idPersons {
-		wg.Add(1)
-		go func(idStr string) {
-			defer wg.Done()
+		pessoasDados, err := api.RetornarPessoaEspecificaGennera([]string{idStr}, tokenGennera)
+		if err != nil {
+			log.Printf("Erro ao obter dados do Gennera para ID %s: %v", idStr, err)
+			idsComErro = append(idsComErro, idStr)
+			continue
+		}
 
-			pessoasDados, err := api.RetornarPessoaEspecificaGennera([]string{idStr}, tokenGennera)
+		for _, pessoa := range pessoasDados {
+			// Adicionando um delay entre cada requisição pode ajudar a evitar atingir limites de taxa
+			time.Sleep(1 * time.Second)
+			clienteOmie := utils.ConverterPessoaParaClienteOmie(pessoa)
+			err := utils.PrepararClienteParaOmie(&clienteOmie)
 			if err != nil {
-				log.Printf("Erro ao obter dados do Gennera para ID %s: %v", idStr, err)
-				mutex.Lock()
+				log.Printf("Erro ao preparar cliente para Omie para ID %s: %v", idStr, err)
+				continue
+			}
+			bodyResponse, err := api.CadastrarClienteOmie(clienteOmie, appKey, appSecret, idStr)
+			if err != nil || bodyResponse == "" {
+				log.Printf("Erro ao cadastrar cliente na Omie para ID %s: %v", idStr, err)
 				idsComErro = append(idsComErro, idStr)
-				mutex.Unlock()
-				return
+			} else {
+				log.Printf("Cliente cadastrado com sucesso na Omie para o ID %s", idStr)
 			}
-
-			for _, pessoa := range pessoasDados {
-				clienteOmie := utils.ConverterPessoaParaClienteOmie(pessoa)
-				//REVIEW - Testar abreviacao de endereco
-				err := utils.PrepararClienteParaOmie(&clienteOmie)
-				if err != nil {
-					log.Printf("Erro ao preparar cliente para Omie para ID %s: %v", idStr, err)
-				}
-				bodyResponse, err := api.CadastrarClienteOmie(clienteOmie, appKey, appSecret, idStr)
-				if err != nil || bodyResponse == "" {
-					log.Printf("Erro ao cadastrar cliente na Omie para ID %s: %v", idStr, err)
-					mutex.Lock()
-					idsComErro = append(idsComErro, idStr)
-					mutex.Unlock()
-				} else {
-					log.Printf("Cliente cadastrado com sucesso na Omie para o ID %s", idStr)
-				}
-			}
-		}(idStr)
+			// Adiciona outro delay após a requisição, se necessário.
+			time.Sleep(1 * time.Second)
+		}
 	}
-	wg.Wait()
 
 	if len(idsComErro) > 0 {
 		utils.SalvarIDsComErro(idsComErro)
